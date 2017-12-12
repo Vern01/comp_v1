1be60f040ceef513b7660b693ee6f1d6e8137c3a
vern01 <verdunw2@gmail.com>
2017-12-12 07:33:05 +0200

    Update Makefile

--- Makefile
+++ Makefile
@@ -2,10 +2,11 @@ MAKEFLAGS += --silent
 
 all:
 	chmod +x computerV1.py
-	echo "Example => ./computerV1.py \"42 + 42X - 42X^2 = 21X\""
+	echo "Example => ./computerV1.py \"42 + 42X - 42X^2 = 21\""
 
 clean:
 	rm -rf __pycache__
+	rm -rf *.pyc
 
 fclean: clean
 
--- computerV1.py
+++ computerV1.py
@@ -57,7 +57,7 @@ def simplify(equation):
 
 
 def main():
-    # try:
+    try:
         equations = purify.equation(sys.argv[1]).split('=')
         a = merge.difference([simplify(eq) for eq in equations])
         a = remove.array_redundant(a)
@@ -74,12 +74,12 @@ def main():
             solve.second_degree(a)
         else:
             print("I cannot solve the polynomial degree of " + str(len(a) - 1))
-    # except Exception:
-    #     print("The format of your input is incorrect. Here are a few tips:\n"
-    #           "\tUse quotation marks around your equation. i.e ./computerV1.py \"42X + 42 = 0\"\n"
-    #           "\tUse the '^' sign to indicate the power of the value X\n"
-    #           "\tMake sure there is a '=' that is either set to 0 or equation of your choice.\n"
-    #           "\tMake sure to remove anything that is not necessary to solve the equation.")
+    except Exception:
+        print("The format of your input is incorrect. Here are a few tips:\n"
+              "\tUse quotation marks around your equation. i.e ./computerV1.py \"42X + 42 = 0\"\n"
+              "\tUse the '^' sign to indicate the power of the value X\n"
+              "\tMake sure there is a '=' that is either set to 0 or equation of your choice.\n"
+              "\tMake sure to remove anything that is not necessary to solve the equation.")
 
 
 if __name__ == '__main__':
